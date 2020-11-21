bd448c36e61d8ed70bef1c70a05ade4c6bfc8d4d
alby <alberto.gomez@socialpubli.com>
2020-11-21 14:19:10 +0100

    feat: fizz-buzz test list numbers

--- fizz-buzz/my-solutions/fizz-buzz.js
+++ fizz-buzz/my-solutions/fizz-buzz.js
@@ -1,4 +1,11 @@
 export function getFizzbuzz(number) {
     let message = []
+    for (let index = 1; index <= number; index++) {
+        switch (true) {
+            default:
+                message.push(index)
+        }
+    }
+
     return message;
 }
--- fizz-buzz/my-solutions/fizz-buzz.spec.js
+++ fizz-buzz/my-solutions/fizz-buzz.spec.js
@@ -6,6 +6,10 @@ describe('addAllNumbers', () => {
 
     const actual = getFizzbuzz(given)
 
-    expect(actual).toEqual([])
+    expect(actual).toEqual([
+        1,2,3,4,5,6,7,8,9,
+        10,11,12,13,14,15,16,17,18
+
+    ])
   })
 })
